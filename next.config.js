0476e8c535d9b64c3389095c7605caca6c4d347a
Aradi <aradi@shinu.app>
2026-04-18 00:00:41 +0300

    fix: mark better-sqlite3 as webpack external for server build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['media.api-sports.io', 'flagcdn.com', 'upload.wikimedia.org'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'better-sqlite3']
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
